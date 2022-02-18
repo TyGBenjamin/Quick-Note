0370560c636c5932d2fb35fb3f04bf07dae49877
Tyler Gandy <tylerbgandy@gmail.com>
2022-02-18 15:36:56 -0500

    changes made to back end to get proper routes working and buttons to add notes

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const api = require('./develop/public/index.js')
+
 // const { clog } = require('./middleware/clog');
-const api = require('./assets/index.js');
+// const api = require('./assets/index.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,18 +17,23 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
-app.use(express.static('public'));
+app.use(express.static('./develop/public'));
+
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/develop/public/index.html'))
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/develop/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/develop/public/notes.html'))
 );
 
 app.listen(PORT, () =>
